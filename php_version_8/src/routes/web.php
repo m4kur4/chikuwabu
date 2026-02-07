2ba43ac8e464021d7738ef07d1522c0d69ec1f34
m4kur4 <ishikawa.016i@gmail.com>
2026-02-07 20:42:31 +0900

    Add SyncGitHubRepositoriesCommand

--- php_version_8/src/routes/web.php
+++ php_version_8/src/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/auth/github', function () {
+    $scopes = ['repo'];
+    return Socialite::driver('github')->scopes($scopes)->redirect();
 });
